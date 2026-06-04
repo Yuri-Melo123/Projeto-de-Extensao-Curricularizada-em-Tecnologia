d4a334a25f58c1eb11282af2e0c25da54c880f68
Yuri-Melo123 <yuri.melo93503@a.fecaf.com.br>
2026-06-04 18:46:59 -0300

    Update api.js

--- social-analytics/mobile/src/services/api.js
+++ social-analytics/mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.15.6:3000", // coloque seu IP local
+  baseURL: "https://social-analytics-api-enx0.onrender.com", // coloque seu IP local
 });
 
 export default api;
